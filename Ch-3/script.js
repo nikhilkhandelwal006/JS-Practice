9995cc50de509a3a9bb40630c1b3ad5abb312295
Nikhil Khandelwal <khandelwalnikhil006@gmail.com>
2024-11-11 12:39:28 +0530

    Functions

--- Ch-3/script.js
+++ Ch-3/script.js
@@ -1,33 +1,25 @@
 //#Functions:-
 
-
 //  It is simply a piece of code which we can use in our program many times.
-
 
 // It is just like a variable holding some piece of code
 
 //// Declaration:-
 
-function nameOffFunction(){
-    console.log("You are running code present inside the function")
+function nameOffFunction() {
+  console.log("You are running code present inside the function");
 }
 
 // function call
-nameOffFunction()
-
-
-
+nameOffFunction();
 
 //Function Expression:-
 
-let fun=function(){
-    console.log("this is an example of function expression")
-}
+let fun = function () {
+  console.log("this is an example of function expression");
+};
 
-fun()
-
-
-
+fun();
 
 // Passing values (Arguments and Parameters):-
 
@@ -38,7 +30,6 @@ fun()
 // invitation("marie")
 // invitation("nikhil")
 
-
 // let invitation=function(name="defaultName", time="night"){
 //     console.log(`welcome! ${name} You are invited on our event at ${time}`)
 // }
@@ -46,22 +37,17 @@ fun()
 // invitation("marie","morning")
 // invitation()
 
-
 // Returning Values(How to return value from the function):-
 
-let ageCalculation=function(birthYear,currentYear=2024){
-    let age=currentYear-birthYear;
-    return age;
-}
+let ageCalculation = function (birthYear, currentYear = 2024) {
+  let age = currentYear - birthYear;
+  return age;
+};
 
 // console.log(ageCalculation(2000))
 
-let functionResult=ageCalculation(2000)
-console.log(`Your age is = ${functionResult}`)
-
-
-
- 
+let functionResult = ageCalculation(2000);
+console.log(`Your age is = ${functionResult}`);
 
 //# Arrow function(ES6):-
 
@@ -71,7 +57,6 @@ console.log(`Your age is = ${functionResult}`)
 // ii.) No need to use paranthesis() in case of single parameter
 //iii.) No need to curely{} if there is single line code in function
 //iv.) No need to use return statement if single line code is in function
-
 
 //Function  expression-->
 // let invitation=function (name) {
@@ -84,13 +69,10 @@ console.log(`Your age is = ${functionResult}`)
 // i)// let invitation=name=> `welcome ${name} to this event`
 // console.log(invitation("coders"))
 
-
 // ii).
-//  let invitation=name=> 
+//  let invitation=name=>
 //     {return `welcome ${name} to this event`}
 // console.log(invitation("coders"))
-
-
 
 //#Passing function as an argument(Higher order function example):-
 
@@ -108,14 +90,12 @@ console.log(`Your age is = ${functionResult}`)
 
 // console.log(transformer("Hello Programmer",lowerCase))
 
-
-
 // Function returning another function
 
-let compliment=function(msg){
-    return function(name){
-        console.log(`${msg} ${name}`)
-    }
+let compliment = function (msg) {
+  return function (name) {
+    console.log(`${msg} ${name}`);
+  };
 };
 
 // compliment("You are a good coder")("NK")   //1st method to do
@@ -125,25 +105,19 @@ let compliment=function(msg){
 
 // compliment is a higher order function
 
-
 //Immediately invoked function expression:-
-
 
 // Execuated only once
 
-(function(name){
-    console.log("this function will never execuate again", name)
-})("IIFE")
-
-
-
+(function (name) {
+  console.log("this function will never execuate again", name);
+})("IIFE");
 
 //#setTimeOut and setInterval:-
 
 // setTimeout -> Run function "once" after "interval" of time
 
 // setInterval -> run function repeatedly, starting  after the interval of time , then repeating .....
-
 
 // 1. setTimeOut
 
@@ -153,20 +127,20 @@ let compliment=function(msg){
 //     console.log(`Welcome ${name} to our coder dost family`)
 // }
 
-// setTimeout(greeting,5000, "Nikhil") 
-
+// setTimeout(greeting,5000, "Nikhil")
 
 // 2. setInterval
 
 // setTimeout(fun|code, delay, arg1, arg2, ...)
 
-function greeting(name){
-    console.log(`Welcome ${name} to our coder dost family`)
+function greeting(name) {
+  console.log(`Welcome ${name} to our coder dost family`);
 }
 
-setInterval(greeting,3000,"Nk")
+setInterval(greeting, 3000, "Nk");
 
 //#Hoisting:-
+
 //variable "declaration" are "hoisted" toward "top" of their scope
 
 // test=6;
@@ -177,14 +151,12 @@ setInterval(greeting,3000,"Nk")
 // console.log(test)     //error
 // let test;
 
-
 //function Declaration
 
 // test()   //Hello Programmers
 // function test(){
 //     console.log("Hello Programmers")
-// }    
-
+// }
 
 //Not function expression or arrow function
 
