de1664506aadfae2356f010ac2c3a2b875d31e59
Nikhil Khandelwal <khandelwalnikhil006@gmail.com>
2024-11-21 17:59:24 +0530

    Array methods

---
+++ Ch-7/script.js
@@ -0,0 +1,27 @@
+//# Working with Arrays(Simple Methods):-
+
+let letters=["a","b","c","d"]
+
+//Reverse (Mutate the original array)-->
+
+let reverse=letters.reverse()
+
+console.log(reverse)
+console.log(letters)
+
+
+
+// concat-->
+
+let nums=[1,2,3,4];
+
+let concated=nums.concat(letters)
+console.log(concated)
+console.log(nums)
+
+
+//Join-->
+
+let joined=letters.join('-')
+console.log(joined)
+// this will convert into string also 
