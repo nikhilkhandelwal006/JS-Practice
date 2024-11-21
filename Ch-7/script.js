446ce42134bd562d6a417d8ac9c18d87c5900b4a
Nikhil Khandelwal <khandelwalnikhil006@gmail.com>
2024-11-21 18:03:27 +0530

    Array methods

--- Ch-7/script.js
+++ Ch-7/script.js
@@ -1,27 +1,33 @@
 //# Working with Arrays(Simple Methods):-
 
-let letters=["a","b","c","d"]
+// let letters=["a","b","c","d"]
 
-//Reverse (Mutate the original array)-->
+// //Reverse (Mutate the original array)-->
 
-let reverse=letters.reverse()
+// let reverse=letters.reverse()
 
-console.log(reverse)
-console.log(letters)
-
-
-
-// concat-->
-
-let nums=[1,2,3,4];
-
-let concated=nums.concat(letters)
-console.log(concated)
-console.log(nums)
+// console.log(reverse)
+// console.log(letters)
 
 
-//Join-->
 
-let joined=letters.join('-')
-console.log(joined)
-// this will convert into string also 
+// // concat-->
+
+// let nums=[1,2,3,4];
+
+// let concated=nums.concat(letters)
+// console.log(concated)
+// console.log(nums)
+
+
+// //Join-->
+
+// let joined=letters.join('-')
+// console.log(joined) // d-c-b-a
+// // this will convert into string also 
+
+
+
+
+// Slice Methods-->
+
