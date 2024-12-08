0735ea16cb8044fe915b5478956b8b78e5a4f868
Nikhil Khandelwal <khandelwalnikhil006@gmail.com>
2024-12-08 11:20:12 +0530

    API Calls

--- Ch-15/index.js
+++ Ch-15/index.js
@@ -1,1 +1,16 @@
-//#Fetch API:
+//# Fetch API:
+
+const URL="https://meowfacts.herokuapp.com/?id=3";
+const facts=document.querySelector("#Facts")
+
+const getFacts=async()=>{
+    console.log("getting data....")
+    let response=await fetch(URL)
+    console.log(response)
+    // console.log(response.status)
+   let data= await response.json();
+//    console.log(data)
+  facts.innerText=data.data[0]
+}
+
+ 
