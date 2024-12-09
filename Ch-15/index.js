d81c3ee84b5451cf9792af451c9e0bd6af740aea
Nikhil Khandelwal <khandelwalnikhil006@gmail.com>
2024-12-09 10:00:49 +0530

    API Calls

--- Ch-15/index.js
+++ Ch-15/index.js
@@ -2,15 +2,36 @@
 
 const URL="https://meowfacts.herokuapp.com/?id=3";
 const facts=document.querySelector("#Facts")
+const btn=document.querySelector("#btn")
 
-const getFacts=async()=>{
-    console.log("getting data....")
-    let response=await fetch(URL)
-    console.log(response)
-    // console.log(response.status)
-   let data= await response.json();
-//    console.log(data)
-  facts.innerText=data.data[0]
+// **This is through async-await(better way)-->
+
+// const getFacts=async()=>{
+//     console.log("getting data....")
+//     let response=await fetch(URL)
+//     console.log(response)
+//     // console.log(response.status)
+//    let data= await response.json();
+// //    console.log(data)
+//   facts.innerText=data.data[0]
+// }
+
+// btn.addEventListener("click",getFacts) 
+
+
+
+
+
+
+// **This is through promise-chaining -->
+
+function getFacts(){
+    fetch(URL).then((response)=>{
+      return response.json()
+    }).then((data)=>{
+        console.log(data)
+        facts.innerText=data.data[0]
+    })
 }
 
- 
+btn.addEventListener("click",getFacts) 
